b9f34a2bd5a46e113aefa4f90fb9b86be2a09b6d
ThanhDuongNguyen <nguyenthanhduonglklklk@gmail.com>
2020-06-28 23:30:01 +0700

    Fix categorie router conflict

--- routers/category.router.js
+++ routers/category.router.js
@@ -1,29 +1,19 @@
-<<<<<<< HEAD
+
 const express = require('express');
 const catergoryModel = require('../models/category.model');
-=======
-const express = require("express");
-const catergoryModel = require("../models/category.model");
->>>>>>> 13400d75523b851c3c5a210b87d0cb88b0b67791
+
 const homeModel = require("../models/home.model");
 
 var router = express.Router();
 
-<<<<<<< HEAD
+
 router.get("/", async function (req, res) {
-    const list = await catergoryModel.all();
-    res.render('viewCategory/list', {
-        categories: list,
-        empty: list.length === 0
-=======
-// 
-router.get("/", async function (req, res) {
-  list = await catergoryModel.all(),
-    res.render("viewCategory/list", {
-      categories: list,
-      empty: list.length === 0,
->>>>>>> 13400d75523b851c3c5a210b87d0cb88b0b67791
-    });
+  const list = await catergoryModel.all();
+  res.render('viewCategory/list', {
+    categories: list,
+    empty: list.length === 0
+
+  });
 });
 
 router.get("/:id", async function (req, res) {
@@ -35,30 +25,31 @@ router.get("/:id", async function (req, res) {
     (listHotnews = await homeModel.hotnewsmenu()),
     (listNews = await catergoryModel.newspaperbyCat(id)),
   ]);
+});
 
-<<<<<<< HEAD
 router.get('/:id', async function (req, res) {
-    var idSeafoodCat = 3;
-    var idAgriculturalCat = 2;
-    const listSeafood = await homeModel.menu(idSeafoodCat);
-    const listAgricultural = await homeModel.menu(idAgriculturalCat);
-    const listHotnews = await homeModel.hotnewsmenu();
-    const id = +req.params.id || -1;
-    const list = await catergoryModel.newspaperbyCat(id);
-    res.render('viewCategory/list', {
-        listNewspaper: list,
-        listSeafood: listSeafood,
-        listAgricultural: listAgricultural,
-        listHotnews: listHotnews,
-    })
+  var idSeafoodCat = 3;
+  var idAgriculturalCat = 2;
+  const listSeafood = await homeModel.menu(idSeafoodCat);
+  const listAgricultural = await homeModel.menu(idAgriculturalCat);
+  const listHotnews = await homeModel.hotnewsmenu();
+  const id = +req.params.id || -1;
+  const list = await catergoryModel.newspaperbyCat(id);
+  res.render('viewCategory/list', {
+    listNewspaper: list,
+    listSeafood: listSeafood,
+    listAgricultural: listAgricultural,
+    listHotnews: listHotnews,
+  })
 })
+
 router.get('/add', function (req, res) {
-    res.render('viewCategoty/add');
+  res.render('viewCategoty/add');
 })
 
 router.post('/add', async function (req, res) {
-    await categoryModel.add(req.body);
-    res.render('viewCategory/add');
+  await categoryModel.add(req.body);
+  res.render('viewCategory/add');
 })
 // router.get('/edit/:id', async function (req, res) {
 //     const id = +req.params.id || -1;
@@ -69,44 +60,12 @@ router.post('/add', async function (req, res) {
 //     res.render('viewCategory/edit', { category });
 // })
 router.post('/del', async function (req, res) {
-    await categoryModel.del(req.body.CatID);
-    res.redirect('/admin/catergories');
+  await categoryModel.del(req.body.CatID);
+  res.redirect('/admin/catergories');
 })
 router.post('/update', async function (req, res) {
-    await categoryModel.patch(req.body);
-    res.redirect('/admin/categories');
-})
-=======
-  res.render("viewCategory/list", {
-    listNewspaper: listNews,
-    listSeafood: listSeafood,
-    listAgricultural: listAgricultural,
-    listHotnews: listHotnews,
-  });
-});
-
-router.get("/add", function (req, res) {
-  res.render("viewCategoty/add");
-});
-
-router.post("/add", async function (req, res) {
-  await categoryModel.add(req.body);
-  res.render("viewCategory/add");
-});
-router.get("/edit/:id", async function (req, res) {
-  const id = +req.params.id || -1;
-  const rowws = await categoryModel.single(id);
-  if (rowws.length === 0) return res.send("Invalid parameter.");
-  const category = rows[0];
-  res.render("viewCategory/edit", { category });
-});
-router.post("/del", async function (req, res) {
-  await categoryModel.del(req.body.CatID);
-  res.redirect("/admin/catergories");
-});
-router.post("/update", async function (req, res) {
   await categoryModel.patch(req.body);
-  res.redirect("/admin/categories");
-});
->>>>>>> 13400d75523b851c3c5a210b87d0cb88b0b67791
+  res.redirect('/admin/categories');
+})
+
 module.exports = router;
